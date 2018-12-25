fb1d52bce2646aeb0bbbffc349c896c3a0417059
Onur Yartaşı <onuryartasi@live.com>
2018-12-25 22:16:13 +0300

    chatbot

--- chatbot.py
+++ chatbot.py
@@ -480,28 +480,49 @@ for epoch in range(1,epochs + 1):
     if early_stopping_check  == early_stopping_stop:
         print("Anymore Cant Improved Chatbot")
         break
-print("Game Over")                    
+print("Game Over")
+
+
+
+# Testing Seq2Seq Model
+
+# Loading weights and Running the session
+
+checkpoint = "./chatbot_weights.ckpt"
+session = tf.InteractiveSession()
+session.run(tf.global_variables_initializer())
+saver = tf.train.Saver()
+saver.restore(session,checkpoint)
+
+#Converting questions from strings to list of encoding integers
+
+def convert_string2int(question,word2int):
+    question = clean_text(question)
+    return [word2int.get(word, word2int['<OUT>']) for word in question.split()]
+ 
+#Setting up chatbot
+while(True):
+    question = input("You: ")
+    if question == 'quit':
+        break
+    question = convert_string2int(question,questionswords2int)
+    question = question + [questionswords2int['<PAD>']*(20-len(question))]
+    fake_batch = np.zeros((batch_size,20))
+    fake_batch[0] = question
+    predicted_answer = session.run(test_predictions,{inputs: fake_batch, keep_prob: 0.5})[0]
+    answer = ''
+    for i in np.argmax(predicted_answer,1):
+        if answersints2word[i] == 'i':
+            token = 'I'
+        elif answersints2word[i] == '<EOS>':
+            token = '.'
+        elif answersints2word[i] == 'OUT':
+            token = 'out'
+        else:
+            token = ' ' + answersints2word[i]
             
-
-
-            
-            
-            
-            
-            
-            
-            
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    
+        answer += token
+        
+        if token == '.'
+            break
+    print('Chatbot: '+ answer)
